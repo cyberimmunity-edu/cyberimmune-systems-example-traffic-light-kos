4004ac3779ad0c30acff2fdeb4992447190b9d22
Sergey Sobolev <sergey.p.sobolev@kaspersky.com>
2024-02-14 15:04:59 +0300

    feat: DHCP client with VFS support integrated
    based on example multi_vfs_dhcpcd implemented another entity Connector with network support
    
    Also refactored EDLs a bit, modified PSL and CMakeLists.txt files to enable new network and vfs modules
    
    Not yet reading data from external server tho

--- Makefile
+++ Makefile
@@ -4,7 +4,6 @@ d-build:
 develop:
 	docker run --net=host --volume="`pwd`:/data" --name demo-e2c -w /data --user user -it --rm kos:1.1.1.40u20.04 bash
 
-
 build-sim:
 	./cross-build.sh
 
--- connector/src/connector.c
+++ connector/src/connector.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+#include <rtl/stdbool.h>
+#include <kos_net.h>
 
 /* Files required for transport initialization. */
 #include <coresrv/nk/transport-kos.h>
@@ -12,16 +19,67 @@
 
 #include <assert.h>
 
+#define DISCOVERING_IFACE_MAX   10
+#define TIME_STEP_SEC           5
+
 #define EXAMPLE_VALUE_TO_SEND 777
 
-/* Client entity entry point. */
+static const char LogPrefix[] = "[Connector]";
+
+/* Connector entity entry point. */
 int main(int argc, const char *argv[])
 {
     NkKosTransport transport;
     struct echo_Ping_proxy proxy;
-    int i;
+    int               i;
+    int               socketfd;
+    struct            ifconf conf;
+    struct            ifreq iface_req[DISCOVERING_IFACE_MAX];
+    struct            ifreq *ifr;
+    struct sockaddr * sa;    
+    bool              is_network_available;
 
-    fprintf(stderr, "Hello I'm connector\n");
+    fprintf(stderr, "%s Hello, I'm about to start working\n", LogPrefix);
+
+    is_network_available = wait_for_network();
+
+    fprintf(stderr, "%s Network status: %s\n", LogPrefix, is_network_available ? "ok" : "not ok");
+    fprintf(stderr, "%s Opening socket...\n", LogPrefix);
+
+    socketfd = socket(AF_ROUTE, SOCK_RAW, 0);
+    if (socketfd < 0)
+    {
+        fprintf(stderr, "\n%s cannot create socket\n", LogPrefix);
+        return EXIT_FAILURE;
+    }
+
+    conf.ifc_len = sizeof(iface_req);
+    conf.ifc_buf = (__caddr_t) iface_req;
+
+    if (ioctl(socketfd,SIOCGIFCONF,&conf) < 0)
+    {
+        fprintf(stderr, "%s ioctl call failed\n", LogPrefix);
+        close(socketfd);
+        return EXIT_FAILURE;
+    }
+
+    fprintf(stderr, "%s Discovering interfaces...\n", LogPrefix);
+
+    for (i = 0; i < conf.ifc_len / sizeof(iface_req[0]); i ++)
+    {
+        ifr = &conf.ifc_req[i];
+        sa = (struct sockaddr *) &ifr->ifr_addr;
+
+        if (sa->sa_family == AF_INET)
+        {
+            struct sockaddr_in *sin = (struct sockaddr_in*) &ifr->ifr_addr;
+
+            fprintf(stderr, "%s %s %s\n",
+                    LogPrefix, ifr->ifr_name, inet_ntoa(sin->sin_addr));
+        }
+    }
+
+    fprintf(stderr, "%s Network check up: OK\n", LogPrefix);
 
     /* Get the client IPC handle of the connection named
      * "server_connection". */
@@ -59,14 +117,14 @@ int main(int argc, const char *argv[])
         {
             /* Print result value from response
              * (result is the output argument of the Ping method). */
-            fprintf(stderr, "result = %d\n", (int) res.result);
+            fprintf(stderr, "%s result = %d\n", LogPrefix, (int) res.result);
             /* Include received result value into value argument
              * to resend to server in next iteration. */
             req.value = res.result;
 
         }
         else
-            fprintf(stderr, "Failed to call echo.Ping.Ping()\n");
+            fprintf(stderr, "%s Failed to call echo.Ping.Ping()\n", LogPrefix);
     }
 
     return EXIT_SUCCESS;
