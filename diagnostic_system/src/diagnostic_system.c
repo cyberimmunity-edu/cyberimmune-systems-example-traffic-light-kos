0d665743bbb79f77f7548ce22fed26a3e9d042d5
Your Name <you@example.com>
2024-02-22 00:46:32 +0300

    tests

--- diagnostic_system/src/diagnostic_system.c
+++ diagnostic_system/src/diagnostic_system.c
@@ -1,123 +1,73 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <stdint.h>
 
 /* Files required for transport initialization. */
 #include <coresrv/nk/transport-kos.h>
 #include <coresrv/sl/sl_api.h>
 
-/* EDL description of the diagnostic_system entity. */
-#include <traffic_light/diagnostic_system.edl.h>
+/* Description of the LightsGPIO interface used by the `DiagnosticSystem` entity. */
+#include <traffic_light/Ping.idl.h>
 
 #include <assert.h>
 
-/* Type of interface implementing object. */
-typedef struct IPingImpl {
-    struct traffic_light_Ping base;     // Base interface of object
-    int step;             // Extra parameters
-} IPingImpl;
+#define EXAMPLE_VALUE_TO_SEND 777
 
-/* Ping method implementation. */
-static nk_err_t Ping_impl(struct traffic_light_Ping *self,
-                          const traffic_light_Ping_req *req,
-                          const struct nk_arena* req_arena,
-                          traffic_light_Ping_res* res,
-                          struct nk_arena* res_arena)
-{
-    IPingImpl *impl = (IPingImpl *)self;
-    /* Increment value in client request by
-     * one step and include into result argument that will be
-     * sent to the client in the diagnostic_system response. */
-    res->Ping.result = req->Ping.value + impl->step;
-    return NK_EOK;
-}
-
-/* IPing object constructor.
- * step is the number by which the input value is increased. */
-static struct traffic_light_Ping *CreateIPingImpl(int step)
-{
-    /* Table of implementations of IPing interface methods. */
-    static const struct traffic_light_Ping_ops ops = {
-        .Ping = Ping_impl
-    };
-
-
-    /* Interface implementing object. */
-    static struct IPingImpl impl = {
-        .base = {&ops}
-    };
-
-    impl.step = step;
-
-    return &impl.base;
-
-}
-
-/* diagnostic_system entry point. */
-int main(void)
+/* DiagnosticSystem entity entry point. */
+int main(int argc, const char *argv[])
 {
     NkKosTransport transport;
-    ServiceId iid;
+    struct traffic_light_Ping_proxy proxy;
+    int i;
 
-    /* Get diagnostic_system IPC handle of "diagnostic_system_connection". */
-    Handle handle = ServiceLocatorRegister("diagnostic_system_connection", NULL, 0, &iid);
+    fprintf(stderr, "Hello I'm DiagnosticSystem\n");
+
+    /* Get the DiagnosticSystem IPC handle of the connection named
+     * "lights_gpio_connection". */
+    Handle handle = ServiceLocatorConnect("lights_gpio_connection");
     assert(handle != INVALID_HANDLE);
 
-    /* Initialize transport to client. */
+    /* Initialize IPC transport for interaction with the LightsGPIO entity. */
     NkKosTransport_Init(&transport, handle, NK_NULL, 0);
 
-    /* Prepare the structures of the request to the diagnostic_system entity: constant
-     * part and arena. Because none of the methods of the diagnostic_system entity has
-     * sequence type arguments, only constant parts of the
-     * request and response are used. Arenas are effectively unused. However,
-     * valid arenas of the request and response must be passed to
-     * diagnostic_system transport methods (nk_transport_recv, nk_transport_reply) and
-     * to the diagnostic_system_entity_dispatch method. */
-    traffic_light_diagnostic_system_entity_req req;
-    char req_buffer[traffic_light_diagnostic_system_entity_req_arena_size];
-    struct nk_arena req_arena = NK_ARENA_INITIALIZER(req_buffer, req_buffer + sizeof(req_buffer));
+    /* Get Runtime Interface ID (RIID) for interface traffic_light.Ping.ping.
+     * Here ping is the name of the traffic_light.Ping component instance,
+     * traffic_light.Ping.ping is the name of the Ping interface implementation. */
+    nk_iid_t riid = ServiceLocatorGetRiid(handle, "traffic_light.Ping.ping");
+    assert(riid != INVALID_RIID);
 
-    /* Prepare response structures: constant part and arena. */
-    traffic_light_diagnostic_system_entity_res res;
-    char res_buffer[traffic_light_diagnostic_system_entity_res_arena_size];
-    struct nk_arena res_arena = NK_ARENA_INITIALIZER(res_buffer, res_buffer + sizeof(res_buffer));
+    /* Initialize proxy object by specifying transport (&transport)
+     * and LightsGPIO interface identifier (riid). Each method of the
+     * proxy object will be implemented by sending a request to the LightsGPIO. */
+    traffic_light_Ping_proxy_init(&proxy, &transport.base, riid);
 
-    /* Initialize ping component dispatcher. 3 is the value of the step,
-     * which is the number by which the input value is increased. */
-    traffic_light_Ping_component component;
-    traffic_light_Ping_component_init(&component, CreateIPingImpl(3));
+    /* Request and response structures */
+    traffic_light_Ping_Ping_req req;
+    traffic_light_Ping_Ping_res res;
 
-    /* Initialize diagnostic_system entity dispatcher. */
-    traffic_light_diagnostic_system_entity entity;
-    traffic_light_diagnostic_system_entity_init(&entity, &component);
-
-    fprintf(stderr, "Hello I'm diagnostic_system\n");
-
-    /* Dispatch loop implementation. */
-    do
+    /* Test loop. */
+    req.value = EXAMPLE_VALUE_TO_SEND;
+    for (i = 0; i < 10; ++i)
     {
-        /* Flush request/response buffers. */
-        nk_req_reset(&req);
-        nk_arena_reset(&req_arena);
-        nk_arena_reset(&res_arena);
+        /* Call Ping interface method.
+         * LightsGPIO will be sent a request for calling Ping interface method
+         * ping_comp.ping_impl with the value argument. Calling thread is locked
+         * until a response is received from the LightsGPIO. */
+        if (traffic_light_Ping_Ping(&proxy.base, &req, NULL, &res, NULL) == rcOk)
 
-        /* Wait for request to diagnostic_system entity. */
-        if (nk_transport_recv(&transport.base, &req.base_, &req_arena) != NK_EOK) {
-            fprintf(stderr, "nk_transport_recv error\n");
-        } else {
-            /* Handle received request by calling implementation Ping_impl
-             * of the requested Ping interface method. */
-            traffic_light_diagnostic_system_entity_dispatch(&entity, &req.base_, &req_arena, &res.base_, &res_arena);
+        {
+            /* Print result value from response
+             * (result is the output argument of the Ping method). */
+            fprintf(stderr, "result = %d\n", (int) res.result);
+            /* Include received result value into value argument
+             * to resend to LightsGPIO in next iteration. */
+            req.value = res.result;
+
         }
-
-
-        /* Send response. */
-        if (nk_transport_reply(&transport.base, &res.base_, &res_arena) != NK_EOK) {
-            fprintf(stderr, "nk_transport_reply error\n");
-        }
+        else
+            fprintf(stderr, "Failed to call traffic_light.Ping.Ping()\n");
     }
-    while (true);
 
     return EXIT_SUCCESS;
 }
