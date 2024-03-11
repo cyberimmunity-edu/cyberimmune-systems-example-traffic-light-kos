80975cfbe1440d33aea617e620af3a8b1f05e4e8
John Doe <johndoe@example.com>
2024-03-11 17:13:11 +0300

    1

--- cross-build.sh
+++ cross-build.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(cd "$(dirname ${0})"; pwd)
-
-BUILD=$(pwd)/build
-mkdir -p $BUILD && cd $BUILD
+SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
+BUILD=${1:-${SCRIPT_DIR}/build}
 
 export LANG=C
 export TARGET="aarch64-kos"
 export PKG_CONFIG=""
 export SDK_PREFIX="/opt/KasperskyOS-Community-Edition-1.1.1.40"
-export INSTALL_PREFIX=$BUILD/../install
-BUILD_SIM_TARGET="y"
+export INSTALL_PREFIX="$BUILD/../install"
 export PATH="$SDK_PREFIX/toolchain/bin:$PATH"
 
 export BUILD_WITH_CLANG=
@@ -26,9 +23,9 @@ if [ "$BUILD_WITH_GCC" == "y" ];then
     TOOLCHAIN_SUFFIX="-gcc"
 fi
 
-$SDK_PREFIX/toolchain/bin/cmake -G "Unix Makefiles" \
+"$SDK_PREFIX/toolchain/bin/cmake" -G "Unix Makefiles" -B "$BUILD" \
       -D CMAKE_BUILD_TYPE:STRING=Debug \
-      -D CMAKE_INSTALL_PREFIX:STRING=$INSTALL_PREFIX \
-      -D CMAKE_FIND_ROOT_PATH="$([[ -f $SCRIPT_DIR/additional_cmake_find_root.txt ]] && cat $SCRIPT_DIR/additional_cmake_find_root.txt)$PREFIX_DIR/sysroot-$TARGET" \
-      -D CMAKE_TOOLCHAIN_FILE=$SDK_PREFIX/toolchain/share/toolchain-$TARGET$TOOLCHAIN_SUFFIX.cmake \
-      $SCRIPT_DIR/ && $SDK_PREFIX/toolchain/bin/cmake --build . --target sim
+      -D CMAKE_INSTALL_PREFIX:STRING="$INSTALL_PREFIX" \
+      -D CMAKE_FIND_ROOT_PATH="$([[ -f "$SCRIPT_DIR/additional_cmake_find_root.txt" ]] && cat "$SCRIPT_DIR/additional_cmake_find_root.txt")$PREFIX_DIR/sysroot-$TARGET" \
+      -D CMAKE_TOOLCHAIN_FILE="$SDK_PREFIX/toolchain/share/toolchain-$TARGET$TOOLCHAIN_SUFFIX.cmake" \
+      "$SCRIPT_DIR/" && "$SDK_PREFIX/toolchain/bin/cmake" --build "$BUILD" --target sim
--- script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$(cd "$(dirname ${0})"; pwd)
-
-BUILD=$(pwd)/build
-mkdir -p $BUILD && cd $BUILD
